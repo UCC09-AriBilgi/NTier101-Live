15d021ef08b74fb3028eb372c37f32c104841bc2
ukaracivio <umit.karacivi@outlook.com>
2023-12-05 12:17:22 +0300

    Some changes

---
+++ NTier101-Live/PL/frmLogin.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace NTier101_Live.PL
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            tboxUserName = new TextBox();
+            label2 = new Label();
+            tboxUserPassw = new TextBox();
+            btonLogin = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Kullanıcı Adı";
+            // 
+            // tboxUserName
+            // 
+            tboxUserName.Location = new Point(103, 17);
+            tboxUserName.MaxLength = 10;
+            tboxUserName.Name = "tboxUserName";
+            tboxUserName.Size = new Size(100, 23);
+            tboxUserName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 62);
+            label2.Name = "label2";
+            label2.Size = new Size(78, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Kullanıcı Şifre";
+            // 
+            // tboxUserPassw
+            // 
+            tboxUserPassw.Location = new Point(103, 59);
+            tboxUserPassw.MaxLength = 10;
+            tboxUserPassw.Name = "tboxUserPassw";
+            tboxUserPassw.Size = new Size(100, 23);
+            tboxUserPassw.TabIndex = 3;
+            // 
+            // btonLogin
+            // 
+            btonLogin.Location = new Point(103, 110);
+            btonLogin.Name = "btonLogin";
+            btonLogin.Size = new Size(75, 23);
+            btonLogin.TabIndex = 4;
+            btonLogin.Text = "Giriş";
+            btonLogin.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(236, 147);
+            ControlBox = false;
+            Controls.Add(btonLogin);
+            Controls.Add(tboxUserPassw);
+            Controls.Add(label2);
+            Controls.Add(tboxUserName);
+            Controls.Add(label1);
+            Name = "frmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Kullanıcı Giriş";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox tboxUserName;
+        private Label label2;
+        private TextBox tboxUserPassw;
+        private Button btonLogin;
+    }
+}
---
+++ NTier101-Live/PL/frmMain.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace NTier101_Live.PL
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btonExit = new Button();
+            SuspendLayout();
+            // 
+            // btonExit
+            // 
+            btonExit.Location = new Point(713, 415);
+            btonExit.Name = "btonExit";
+            btonExit.Size = new Size(75, 23);
+            btonExit.TabIndex = 0;
+            btonExit.Text = "Çıkış";
+            btonExit.UseVisualStyleBackColor = true;
+            btonExit.Click += btonExit_Click;
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btonExit);
+            Name = "frmMain";
+            Text = "Ana Ekran";
+            WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btonExit;
+    }
+}
