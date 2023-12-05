43ed8c315af6c13116f74cfac579831e62d05504
ukaracivio <umit.karacivi@outlook.com>
2023-12-05 12:06:57 +0300

    Some changes

---
+++ NTier101-Live/DAL/datUserDAO.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier101_Live.DAL
+{
+    internal class datUserDAO
+    {
+        private clsDBOperation clsDBOperation;
+
+        // constructor
+        public datUserDAO()
+        {
+            clsDBOperation = new clsDBOperation();
+        }
+
+        // Bir kullanıcının ceşitli bilgilerinin searchj edileceği bir durum
+        // Sisteme giriş için kullanılacak
+
+        public DataTable searchUserIDPassw(string userName,string userPassw)
+        {
+            string Query = string.Format("SELECT * FROM datUser WHERE UserName=@UserName AND UserPassw=@UserPassw");
+
+            SqlParameter[] sqlParameters=new SqlParameter[2];
+
+            // Tanımlanıyor
+            sqlParameters[0]= new SqlParameter("UserName",SqlDbType.NVarChar);
+            sqlParameters[1] = new SqlParameter("UserPassw", SqlDbType.NVarChar);
+
+            // Değerler atanıyor
+            sqlParameters[0].Value = userName;
+            sqlParameters[1].Value = userPassw;
+
+            return clsDBOperation.executeSelectQuery(Query,sqlParameters);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ NTier101-Live/PocosL/datUserPoco.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier101_Live.PocosL
+{
+    internal class datUserPoco
+    {
+        private int UserID { get; set; }
+        private string UserName { get; set; }
+        private string UserPassw { get; set; }
+        private string Ad { get; set; }
+        private string Soyad { get; set; }
+        private string TCKimlik { get; set; }
+        private int MudurlukID { get; set; }
+
+
+
+
+
+    }
+}
