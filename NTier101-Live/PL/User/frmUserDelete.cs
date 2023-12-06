ca09ac2af56eaa913ebfcdf11280acee4b1c30c1
ukaracivio <umit.karacivi@outlook.com>
2023-12-06 10:49:43 +0300

    Some changes

---
+++ NTier101-Live/PL/User/frmUserDelete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NTier101_Live.PL.User
+{
+    public partial class frmUserDelete : Form
+    {
+        public frmUserDelete()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- NTier101-Live/PL/frmMain.Designer.cs
+++ NTier101-Live/PL/frmMain.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             btonExit = new Button();
+            menuNTier = new MenuStrip();
+            kullanıcıToolStripMenuItem = new ToolStripMenuItem();
+            yeniToolStripMenuItem = new ToolStripMenuItem();
+            silToolStripMenuItem = new ToolStripMenuItem();
+            güncelleToolStripMenuItem = new ToolStripMenuItem();
+            menuNTier.SuspendLayout();
             SuspendLayout();
             // 
             // btonExit
@@ -42,20 +48,64 @@
             btonExit.UseVisualStyleBackColor = true;
             btonExit.Click += btonExit_Click;
             // 
+            // menuNTier
+            // 
+            menuNTier.Items.AddRange(new ToolStripItem[] { kullanıcıToolStripMenuItem });
+            menuNTier.Location = new Point(0, 0);
+            menuNTier.Name = "menuNTier";
+            menuNTier.Size = new Size(819, 24);
+            menuNTier.TabIndex = 1;
+            menuNTier.Text = "menuStrip1";
+            // 
+            // kullanıcıToolStripMenuItem
+            // 
+            kullanıcıToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { yeniToolStripMenuItem, silToolStripMenuItem, güncelleToolStripMenuItem });
+            kullanıcıToolStripMenuItem.Name = "kullanıcıToolStripMenuItem";
+            kullanıcıToolStripMenuItem.Size = new Size(64, 20);
+            kullanıcıToolStripMenuItem.Text = "Kullanıcı";
+            // 
+            // yeniToolStripMenuItem
+            // 
+            yeniToolStripMenuItem.Name = "yeniToolStripMenuItem";
+            yeniToolStripMenuItem.Size = new Size(180, 22);
+            yeniToolStripMenuItem.Text = "Yeni";
+            // 
+            // silToolStripMenuItem
+            // 
+            silToolStripMenuItem.Name = "silToolStripMenuItem";
+            silToolStripMenuItem.Size = new Size(180, 22);
+            silToolStripMenuItem.Text = "Sil";
+            // 
+            // güncelleToolStripMenuItem
+            // 
+            güncelleToolStripMenuItem.Name = "güncelleToolStripMenuItem";
+            güncelleToolStripMenuItem.Size = new Size(180, 22);
+            güncelleToolStripMenuItem.Text = "Güncelle";
+            // 
             // frmMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(819, 450);
             Controls.Add(btonExit);
+            Controls.Add(menuNTier);
+            MainMenuStrip = menuNTier;
             Name = "frmMain";
             Text = "Ana Ekran";
             WindowState = FormWindowState.Maximized;
+            menuNTier.ResumeLayout(false);
+            menuNTier.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button btonExit;
+        private MenuStrip menuNTier;
+        private ToolStripMenuItem kullanıcıToolStripMenuItem;
+        private ToolStripMenuItem yeniToolStripMenuItem;
+        private ToolStripMenuItem silToolStripMenuItem;
+        private ToolStripMenuItem güncelleToolStripMenuItem;
     }
 }
