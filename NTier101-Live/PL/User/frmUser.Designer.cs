e4177c4bfa50dc182540dfc87172fef98bf89f2a
ukaracivio <umit.karacivi@outlook.com>
2023-12-06 13:29:59 +0300

    Some changes

--- NTier101-Live/PL/User/frmUser.Designer.cs
+++ NTier101-Live/PL/User/frmUser.Designer.cs
@@ -40,14 +40,25 @@
             tboxMudurlukID = new TextBox();
             tboxTCKimlik = new TextBox();
             btonClose = new Button();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            groupBox2 = new GroupBox();
             ((System.ComponentModel.ISupportInitialize)dgrdUser).BeginInit();
             groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // dgrdUser
             // 
             dgrdUser.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgrdUser.Location = new Point(12, 12);
+            dgrdUser.Location = new Point(133, 41);
             dgrdUser.Name = "dgrdUser";
             dgrdUser.RowTemplate.Height = 25;
             dgrdUser.Size = new Size(479, 150);
@@ -55,25 +66,29 @@
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(btonDelete);
-            groupBox1.Controls.Add(btonUpdate);
+            groupBox1.Controls.Add(groupBox2);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(tboxUserName);
             groupBox1.Controls.Add(tboxUserPassw);
             groupBox1.Controls.Add(tboxAd);
-            groupBox1.Controls.Add(btonSave);
             groupBox1.Controls.Add(tboxSoyad);
             groupBox1.Controls.Add(tboxMudurlukID);
             groupBox1.Controls.Add(tboxTCKimlik);
-            groupBox1.Location = new Point(12, 168);
+            groupBox1.Location = new Point(133, 197);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(479, 196);
+            groupBox1.Size = new Size(479, 204);
             groupBox1.TabIndex = 10;
             groupBox1.TabStop = false;
             groupBox1.Text = "Kullanıcı Detay";
             // 
             // btonDelete
             // 
-            btonDelete.Location = new Point(322, 167);
+            btonDelete.Location = new Point(16, 88);
             btonDelete.Name = "btonDelete";
             btonDelete.Size = new Size(75, 23);
             btonDelete.TabIndex = 8;
@@ -82,7 +97,7 @@
             // 
             // btonUpdate
             // 
-            btonUpdate.Location = new Point(174, 167);
+            btonUpdate.Location = new Point(16, 56);
             btonUpdate.Name = "btonUpdate";
             btonUpdate.Size = new Size(75, 23);
             btonUpdate.TabIndex = 7;
@@ -91,7 +106,7 @@
             // 
             // tboxUserName
             // 
-            tboxUserName.Location = new Point(60, 32);
+            tboxUserName.Location = new Point(105, 32);
             tboxUserName.MaxLength = 10;
             tboxUserName.Name = "tboxUserName";
             tboxUserName.Size = new Size(100, 23);
@@ -99,7 +114,7 @@
             // 
             // tboxUserPassw
             // 
-            tboxUserPassw.Location = new Point(60, 61);
+            tboxUserPassw.Location = new Point(319, 32);
             tboxUserPassw.MaxLength = 10;
             tboxUserPassw.Name = "tboxUserPassw";
             tboxUserPassw.Size = new Size(100, 23);
@@ -107,14 +122,14 @@
             // 
             // tboxAd
             // 
-            tboxAd.Location = new Point(255, 32);
+            tboxAd.Location = new Point(105, 72);
             tboxAd.Name = "tboxAd";
             tboxAd.Size = new Size(142, 23);
             tboxAd.TabIndex = 2;
             // 
             // btonSave
             // 
-            btonSave.Location = new Point(26, 167);
+            btonSave.Location = new Point(16, 22);
             btonSave.Name = "btonSave";
             btonSave.Size = new Size(75, 23);
             btonSave.TabIndex = 6;
@@ -123,21 +138,21 @@
             // 
             // tboxSoyad
             // 
-            tboxSoyad.Location = new Point(255, 61);
+            tboxSoyad.Location = new Point(105, 101);
             tboxSoyad.Name = "tboxSoyad";
             tboxSoyad.Size = new Size(142, 23);
             tboxSoyad.TabIndex = 3;
             // 
             // tboxMudurlukID
             // 
-            tboxMudurlukID.Location = new Point(255, 117);
+            tboxMudurlukID.Location = new Point(105, 159);
             tboxMudurlukID.Name = "tboxMudurlukID";
             tboxMudurlukID.Size = new Size(32, 23);
             tboxMudurlukID.TabIndex = 5;
             // 
             // tboxTCKimlik
             // 
-            tboxTCKimlik.Location = new Point(255, 90);
+            tboxTCKimlik.Location = new Point(105, 130);
             tboxTCKimlik.MaxLength = 11;
             tboxTCKimlik.Name = "tboxTCKimlik";
             tboxTCKimlik.Size = new Size(100, 23);
@@ -146,7 +161,7 @@
             // btonClose
             // 
             btonClose.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            btonClose.Location = new Point(414, 380);
+            btonClose.Location = new Point(544, 407);
             btonClose.Name = "btonClose";
             btonClose.Size = new Size(75, 23);
             btonClose.TabIndex = 9;
@@ -154,21 +169,119 @@
             btonClose.UseVisualStyleBackColor = true;
             btonClose.Click += btonClose_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(133, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(150, 21);
+            label1.TabIndex = 11;
+            label1.Text = "Kayıtlı Kullanıcılar";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.kullaniciGirisi;
+            pictureBox1.Location = new Point(12, 9);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 93);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 12;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(16, 35);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 15);
+            label2.TabIndex = 13;
+            label2.Text = "Kullanıcı Adı";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(230, 35);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 15);
+            label3.TabIndex = 14;
+            label3.Text = "Kullanıcı Şifre";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(16, 75);
+            label4.Name = "label4";
+            label4.Size = new Size(22, 15);
+            label4.TabIndex = 15;
+            label4.Text = "Ad";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(16, 104);
+            label5.Name = "label5";
+            label5.Size = new Size(40, 15);
+            label5.TabIndex = 16;
+            label5.Text = "Soyad";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(16, 133);
+            label6.Name = "label6";
+            label6.Size = new Size(78, 15);
+            label6.TabIndex = 17;
+            label6.Text = "TC Kimlik No";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(16, 162);
+            label7.Name = "label7";
+            label7.Size = new Size(61, 15);
+            label7.TabIndex = 18;
+            label7.Text = "Müdürlük";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btonSave);
+            groupBox2.Controls.Add(btonUpdate);
+            groupBox2.Controls.Add(btonDelete);
+            groupBox2.Location = new Point(368, 67);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(105, 129);
+            groupBox2.TabIndex = 19;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "İşlemler";
+            // 
             // frmUser
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(501, 415);
+            ClientSize = new Size(631, 434);
+            ControlBox = false;
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
             Controls.Add(btonClose);
             Controls.Add(groupBox1);
             Controls.Add(dgrdUser);
             Name = "frmUser";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "frmUser";
+            Text = "Kullanıcı Bilgileri";
             ((System.ComponentModel.ISupportInitialize)dgrdUser).EndInit();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox2.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -185,5 +298,14 @@
         private TextBox tboxMudurlukID;
         private TextBox tboxTCKimlik;
         private Button btonClose;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private GroupBox groupBox2;
     }
 }
