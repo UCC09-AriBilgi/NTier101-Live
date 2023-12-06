4910e19faf1fc9c0d1ca0fe0adfeb9e58151dac8
ukaracivio <umit.karacivi@outlook.com>
2023-12-06 12:14:44 +0300

    Some changes

--- NTier101-Live/PL/User/frmUser.Designer.cs
+++ NTier101-Live/PL/User/frmUser.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NTier101_Live.PL.User
 {
-    partial class frmUserDelete
+    partial class frmUser
     {
         /// <summary>
         /// Required designer variable.
@@ -30,6 +30,8 @@
         {
             dgrdUser = new DataGridView();
             groupBox1 = new GroupBox();
+            btonDelete = new Button();
+            btonUpdate = new Button();
             tboxUserName = new TextBox();
             tboxUserPassw = new TextBox();
             tboxAd = new TextBox();
@@ -37,6 +39,7 @@
             tboxSoyad = new TextBox();
             tboxMudurlukID = new TextBox();
             tboxTCKimlik = new TextBox();
+            btonClose = new Button();
             ((System.ComponentModel.ISupportInitialize)dgrdUser).BeginInit();
             groupBox1.SuspendLayout();
             SuspendLayout();
@@ -47,11 +50,13 @@
             dgrdUser.Location = new Point(12, 12);
             dgrdUser.Name = "dgrdUser";
             dgrdUser.RowTemplate.Height = 25;
-            dgrdUser.Size = new Size(405, 150);
+            dgrdUser.Size = new Size(479, 150);
             dgrdUser.TabIndex = 0;
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(btonDelete);
+            groupBox1.Controls.Add(btonUpdate);
             groupBox1.Controls.Add(tboxUserName);
             groupBox1.Controls.Add(tboxUserPassw);
             groupBox1.Controls.Add(tboxAd);
@@ -65,6 +70,24 @@
             groupBox1.TabIndex = 10;
             groupBox1.TabStop = false;
             groupBox1.Text = "Kullanıcı Detay";
+            // 
+            // btonDelete
+            // 
+            btonDelete.Location = new Point(322, 167);
+            btonDelete.Name = "btonDelete";
+            btonDelete.Size = new Size(75, 23);
+            btonDelete.TabIndex = 8;
+            btonDelete.Text = "Sil";
+            btonDelete.UseVisualStyleBackColor = true;
+            // 
+            // btonUpdate
+            // 
+            btonUpdate.Location = new Point(174, 167);
+            btonUpdate.Name = "btonUpdate";
+            btonUpdate.Size = new Size(75, 23);
+            btonUpdate.TabIndex = 7;
+            btonUpdate.Text = "Güncelle";
+            btonUpdate.UseVisualStyleBackColor = true;
             // 
             // tboxUserName
             // 
@@ -91,11 +114,11 @@
             // 
             // btonSave
             // 
-            btonSave.Location = new Point(398, 167);
+            btonSave.Location = new Point(26, 167);
             btonSave.Name = "btonSave";
             btonSave.Size = new Size(75, 23);
             btonSave.TabIndex = 6;
-            btonSave.Text = "Kayıt";
+            btonSave.Text = "Yeni";
             btonSave.UseVisualStyleBackColor = true;
             // 
             // tboxSoyad
@@ -120,18 +143,28 @@
             tboxTCKimlik.Size = new Size(100, 23);
             tboxTCKimlik.TabIndex = 4;
             // 
-            // frmUserDelete
+            // btonClose
             // 
-            AccessibleRole = AccessibleRole.PageTabList;
+            btonClose.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btonClose.Location = new Point(414, 380);
+            btonClose.Name = "btonClose";
+            btonClose.Size = new Size(75, 23);
+            btonClose.TabIndex = 9;
+            btonClose.Text = "Kapat";
+            btonClose.UseVisualStyleBackColor = true;
+            btonClose.Click += btonClose_Click;
+            // 
+            // frmUser
+            // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(628, 413);
-            ControlBox = false;
+            ClientSize = new Size(501, 415);
+            Controls.Add(btonClose);
             Controls.Add(groupBox1);
             Controls.Add(dgrdUser);
-            Name = "frmUserDelete";
+            Name = "frmUser";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "frmUserDelete";
+            Text = "frmUser";
             ((System.ComponentModel.ISupportInitialize)dgrdUser).EndInit();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
@@ -142,6 +175,8 @@
 
         private DataGridView dgrdUser;
         private GroupBox groupBox1;
+        private Button btonDelete;
+        private Button btonUpdate;
         private TextBox tboxUserName;
         private TextBox tboxUserPassw;
         private TextBox tboxAd;
@@ -149,5 +184,6 @@
         private TextBox tboxSoyad;
         private TextBox tboxMudurlukID;
         private TextBox tboxTCKimlik;
+        private Button btonClose;
     }
 }
