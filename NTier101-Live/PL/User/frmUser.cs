373cb084b7ef8c9b6ca33b454193423061278ff0
ukaracivio <umit.karacivi@outlook.com>
2023-12-07 13:47:11 +0300

    Some changes

--- NTier101-Live/PL/User/frmUser.cs
+++ NTier101-Live/PL/User/frmUser.cs
@@ -42,157 +42,13 @@ namespace NTier101_Live.PL.User
 
         }
 
-        public void FormInit()
-        {
-            Mode = "";
-
-            tboxUserName.Clear();
-            tboxUserPassw.Clear();
-            tboxAd.Clear();
-            tboxSoyad.Clear();
-            tboxTCKimlik.Clear();
-            tboxMudurlukID.Clear();
-
-            grpbUser.Enabled = false;
-
-            ShowRecords();
-        }
-
         #endregion
 
         #region Button Events
-
-
-        #endregion
-
-        #region DataGrid Events
-
-
-        #endregion
-
-        #region General Methods
-
-
-        #endregion
-
+        
         private void btonClose_Click(object sender, EventArgs e)
         {
             this.Close();
-        }
-
-
-
-
-        // DG yi dolduracak olan metot
-        void ShowRecords()
-        {
-            clsDBOperation conn = new clsDBOperation();
-
-            query = new SqlCommand("SELECT * FROM datUser", conn.Connection);
-
-            DataTable dataTable = new DataTable();
-
-            SqlDataAdapter adapter = new SqlDataAdapter(query);
-
-            adapter.Fill(dataTable);
-
-            BindingSource bindingSource = new BindingSource();
-
-            bindingSource.DataSource = dataTable;
-
-            dgrdUser.DataSource = bindingSource;
-
-
-        }
-
-        private void dgrdUser_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
-        {
-            //query = new SqlCommand("SELECT * FROM datUser WHERE UserID=" + dgrdUser.CurrentRow.Cells[0].Value, clsDBOperation.Connection);
-
-            grpbUser.Enabled = true;
-
-            tboxUserName.Text = "";
-            tboxUserPassw.Text = "";
-            tboxAd.Text = "";
-            tboxSoyad.Text = "";
-            tboxTCKimlik.Text = "";
-            tboxMudurlukID.Text = "";
-
-
-            // dg üzerinde herhangi bir satırda çift tıklama ile forma gitme...
-            //ShowData("U"); // 3 ayrı işlemi (I,U,D) tek metotda birleştiren benim yazdığım metot
-            //BindGrid();
-        }
-
-        private void BindGrid()
-        {
-            // VT nındaki bilgileri alıp DG içine gömmek için
-
-            //using (SqlConnection connection = new SqlConnection(@"Data Source=BILCE\\ARIBILGI;Initial Catalog=Northwind;TrustServerCertificate=True;Persist Security Info=True;Encrypt=False;"))
-
-            string vs_SQLCommand = "SELECT * FROM dat_User";
-
-            using (SqlConnection connection = new SqlConnection(@"Data Source=BABEGT\SQLEXPRESS;Initial Catalog=NTier101Db;Integrated Security=True;TrustServerCertificate=True;"))
-            {
-                using (SqlCommand command = new SqlCommand(vs_SQLCommand, connection))
-                {
-                    command.CommandType = CommandType.Text;
-
-                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
-                    {
-                        using (DataSet dset = new DataSet())
-                        {
-                            adapter.Fill(dset);
-
-                            dgrdUser.DataSource = dset.Tables[0];
-
-                        }
-                    }
-                }
-
-            }
-        }
-
-
-        private void ShowData(string prmMode)
-        {
-            //frmADO_Detail frmADO_Detail = new frmADO_Detail();
-
-            //frmADO_Detail.Mod = prmMode;
-
-            //// IUD durumuna göre ekran komponent ayarları yapılıyor
-
-            //switch (prmMode)
-            //{
-            //    case "I": // Insert Modu için
-            //        frmADO_Detail.tboxCustomerID.Enabled = true;
-            //        frmADO_Detail.tboxCustomerID.Text = "";
-            //        frmADO_Detail.tboxCompanyName.Text = "";
-            //        frmADO_Detail.tboxContactName.Text = "";
-            //        frmADO_Detail.tboxCountry.Text = "";
-
-            //        break;
-
-            //    case "U": // Update Modu için
-            //        frmADO_Detail.tboxCustomerID.Enabled = false;
-            //        // dg üzerindeki ilgili alanlardaki veriler detay formunun ilgili yerlerine yerleştiriliyor
-            //        frmADO_Detail.tboxCustomerID.Text = dgrdCustomers.CurrentRow.Cells[0].Value.ToString();
-            //        frmADO_Detail.tboxCompanyName.Text = dgrdCustomers.CurrentRow.Cells[1].Value.ToString();
-            //        frmADO_Detail.tboxContactName.Text = dgrdCustomers.CurrentRow.Cells[2].Value.ToString();
-            //        frmADO_Detail.tboxCountry.Text = dgrdCustomers.CurrentRow.Cells[3].Value.ToString();
-
-            //        break;
-
-            //    default:
-            //        break;
-            //}
-
-            //frmADO_Detail.ShowDialog();
-        }
-
-        private void dgrdUser_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
         }
 
         private void btonSave_Click(object sender, EventArgs e)
@@ -210,22 +66,7 @@ namespace NTier101_Live.PL.User
             MessageBox.Show("Kullanıcı kaydınız yapılmıştır...");
 
             Temizle();// ekrandaki eski bilgiler temizleniyor.
-            BindGrid(); // Gridi yeni datayla doldurmak için
-
-        }
-
-        void Temizle()
-        {
-            tboxUserName.Clear();
-            tboxUserPassw.Clear();
-            tboxAd.Clear();
-            tboxSoyad.Clear();
-            tboxTCKimlik.Clear();
-            tboxMudurlukID.Clear();
-        }
-
-        private void pboxNew_DoubleClick(object sender, EventArgs e)
-        {
+            ShowRecords(); // Gridi yeni datayla doldurmak için
 
         }
 
@@ -236,43 +77,6 @@ namespace NTier101_Live.PL.User
             grpbUser.Enabled = true;
 
             tboxUserName.Focus(); // fare imlecini ilgili kontrole koyuyor.
-        }
-
-
-        private void IUD()
-        {
-            datUserPoco datUserPoco = new datUserPoco();
-
-            switch (Mode)
-            {
-                case "I":
-                    datUserPoco = datUserBLL.insertUser(tboxUserName.Text, tboxUserPassw.Text, tboxAd.Text, tboxSoyad.Text, tboxTCKimlik.Text, Convert.ToInt32(tboxMudurlukID.Text));
-
-                    MessageBox.Show("Kayıt işlemi başarılı...");
-
-                    break;
-
-                case "U":
-                    datUserPoco = datUserBLL.updateUser(Convert.ToInt32(dgrdUser.CurrentRow.Cells[0].Value), tboxUserName.Text, tboxUserPassw.Text, tboxAd.Text, tboxSoyad.Text, tboxTCKimlik.Text, Convert.ToInt32(tboxMudurlukID.Text));
-
-                    MessageBox.Show("Güncelleme işlemi başarılı...");
-
-                    break;
-
-                case "D":
-                    datUserPoco = datUserBLL.deleteUser(Convert.ToInt32(dgrdUser.CurrentRow.Cells[0].Value));
-
-                    MessageBox.Show("Silme işlemi başarılı...");
-
-                    break;
-
-                default:
-                    break;
-            }
-
-            Temizle();
-
-            ShowRecords();
         }
 
         private void btonSave_Click_1(object sender, EventArgs e)
@@ -313,5 +117,142 @@ namespace NTier101_Live.PL.User
             IUD();
 
         }
+        
+        #endregion
+
+        #region DataGrid Events
+        
+        private void dgrdUser_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //query = new SqlCommand("SELECT * FROM datUser WHERE UserID=" + dgrdUser.CurrentRow.Cells[0].Value, clsDBOperation.Connection);
+
+            grpbUser.Enabled = true;
+
+            tboxUserName.Text = "";
+            tboxUserPassw.Text = "";
+            tboxAd.Text = "";
+            tboxSoyad.Text = "";
+            tboxTCKimlik.Text = "";
+            tboxMudurlukID.Text = "";
+
+
+            // dg üzerinde herhangi bir satırda çift tıklama ile forma gitme...
+            //ShowData("U"); // 3 ayrı işlemi (I,U,D) tek metotda birleştiren benim yazdığım metot
+            //BindGrid();
+        }
+
+
+
+        private void dgrdUser_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region General Methods
+        // DG yi dolduracak olan metot
+        void ShowRecords()
+        {
+            clsDBOperation conn = new clsDBOperation();
+
+            query = new SqlCommand("SELECT * FROM datUser", conn.Connection);
+
+            DataTable dataTable = new DataTable();
+
+            SqlDataAdapter adapter = new SqlDataAdapter(query);
+
+            adapter.Fill(dataTable);
+
+            BindingSource bindingSource = new BindingSource();
+
+            bindingSource.DataSource = dataTable;
+
+            dgrdUser.DataSource = bindingSource;
+
+
+        }
+
+        // Form ilk açıldığında gerekli ayarları yapıyor.
+        public void FormInit()
+        {
+            Mode = "";
+
+            tboxUserName.Clear();
+            tboxUserPassw.Clear();
+            tboxAd.Clear();
+            tboxSoyad.Clear();
+            tboxTCKimlik.Clear();
+            tboxMudurlukID.Clear();
+
+            grpbUser.Enabled = false;
+
+            ShowRecords();
+        }
+
+        void Temizle()
+        {
+            tboxUserName.Clear();
+            tboxUserPassw.Clear();
+            tboxAd.Clear();
+            tboxSoyad.Clear();
+            tboxTCKimlik.Clear();
+            tboxMudurlukID.Clear();
+        }
+
+        // I U D işlemlerini tek bir metotda hallediyor
+        private void IUD()
+        {
+            datUserPoco datUserPoco = new datUserPoco();
+
+            switch (Mode)
+            {
+                case "I":
+                    datUserPoco = datUserBLL.insertUser(tboxUserName.Text, tboxUserPassw.Text, tboxAd.Text, tboxSoyad.Text, tboxTCKimlik.Text, Convert.ToInt32(tboxMudurlukID.Text));
+
+                    MessageBox.Show("Kayıt işlemi başarılı...");
+
+                    break;
+
+                case "U":
+                    datUserPoco = datUserBLL.updateUser(Convert.ToInt32(dgrdUser.CurrentRow.Cells[0].Value), tboxUserName.Text, tboxUserPassw.Text, tboxAd.Text, tboxSoyad.Text, tboxTCKimlik.Text, Convert.ToInt32(tboxMudurlukID.Text));
+
+                    MessageBox.Show("Güncelleme işlemi başarılı...");
+
+                    break;
+
+                case "D":
+                    datUserPoco = datUserBLL.deleteUser(Convert.ToInt32(dgrdUser.CurrentRow.Cells[0].Value));
+
+                    MessageBox.Show("Silme işlemi başarılı...");
+
+                    break;
+
+                default:
+                    break;
+            }
+
+            Temizle();
+
+            ShowRecords();
+        }
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
