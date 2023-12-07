61250bd3621ddc1dc3b237b2b7b5c6510afcea92
ukaracivio <umit.karacivi@outlook.com>
2023-12-07 10:31:22 +0300

    Some changes

--- NTier101-Live/BLL/datUserBLL.cs
+++ NTier101-Live/BLL/datUserBLL.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 
 namespace NTier101_Live.BLL
 {
+    // Bu class yönlendirici bir classdır. Yani bir trafik polisi gibi.Sen şu işi yapacaksan şuraya, bu işi yapacaksan buraya git gibi yönlendirmeleri yapıyor. İlerde bunu Controller(MVC tarafında) olarak adlandıracağız.
+    
     public class datUserBLL
     {
         private datUserDAO datUserDAO;
@@ -20,6 +22,30 @@ namespace NTier101_Live.BLL
         }
 
         // Parametrik sorgulama sonrası istenen kullanıcı kaydını getirme
+        public datUserPoco getUser(int UserID)
+        {
+            datUserPoco datUserPoco = new datUserPoco();
+
+            DataTable dataTable = new DataTable();
+
+            // Data Access yapan objeme git . Oradaki getUser isimli metodu calıtır.
+            dataTable = datUserDAO.getUser(UserID);
+
+            // gelen datatable nesnesindeki propertyleri ilgili datUserPoco isimli clasın propertylerine gececek ki diğer classlarda da kullanabileyim.
+            foreach (DataRow row in dataTable.Rows)
+            {
+                datUserPoco.UserName = row["UserName"].ToString();
+                datUserPoco.UserPassw = row["UserPassw"].ToString();
+                datUserPoco.Ad = row["Ad"].ToString();
+                datUserPoco.Soyad = row["Soyad"].ToString();
+                datUserPoco.TCKimlik = row["TCKimlik"].ToString();
+                datUserPoco.MudurlukID = Convert.ToInt32(row["MudurlukID"].ToString());
+
+            }
+
+            return datUserPoco; // Çağıran yere classı gönder
+
+        }
 
 
         // Login ekranından girilen bilgilere göre datUser tablosunda uygun bir kayıt var/yok?
@@ -40,6 +66,7 @@ namespace NTier101_Live.BLL
             return datUserPoco; // varsa çağıran katmana gönder
         } 
 
+        // DAO sınıfındaki işi yapacak ilgili metodu çağırıyor
         public datUserPoco insertUser(string UserName, string UserPassw,string Ad,string Soyad,string TCKimlik,int MudurlukID)
         {
             datUserPoco datUserPoco= new datUserPoco();
@@ -58,6 +85,32 @@ namespace NTier101_Live.BLL
             }
 
             return datUserPoco;
+        }
+
+        public datUserPoco updateUser(int UserID, string UserName, string UserPassw, string Ad, string Soyad, string TCKimlik, int MudurlukID)
+        {
+            datUserPoco datUserPoco = new datUserPoco();
+
+            DataTable dataTable =new DataTable();
+
+            datUserDAO.updateUser(UserID, UserName, UserPassw, Ad, Soyad, TCKimlik, MudurlukID);
+
+
+            return datUserPoco;
+
+        }
+
+        public datUserPoco deleteUser (int UserID) 
+        {
+            datUserPoco datUserPoco = new datUserPoco();
+
+            DataTable dataTable = new DataTable();
+
+            datUserDAO.deleteUser(UserID);
+
+
+            return datUserPoco;
+
         }
     }
 }
